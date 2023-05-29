45df867410673079522ed40cc1f42ec660c59556
Kusochec <xelmasterz@gmail.com>
2023-05-30 00:36:00 +0300

    cheing r1.2

--- lab1/R1.2.js
+++ lab1/R1.2.js
@@ -1,2 +1,18 @@
 //Таблица умножения NxN размера указаного в пораметре
-multiplicationTable(3) // ->[[1,2,3],[2,4,6],[3,6,9]]
+//multiplicationTable(3)  ->[[1,2,3],[2,4,6],[3,6,9]]
+
+let arr = new Array();
+let table = new Array();
+let num = prompt("Введите число:  ")
+
+function mult() {
+
+    for (let i = 0; i < num; i++) {
+        arr[i] = i+1;
+        
+      }
+      console.log(arr);
+  }
+
+  mult();
+  
