995eb903b09c07c15c293132577b44d16d5bffbc
Kusochec <xelmasterz@gmail.com>
2023-06-01 13:21:27 +0300

    r1.2 end; fix r1.1

--- lab1/R1.1.js
+++ lab1/R1.1.js
@@ -1,43 +1,38 @@
-
-// "A": "1",
-//  "E": "5",
-//  "I": "9",
-//  "O": "15",
-// "U": "21",
-// "Y": "25",
-let num = [1, 5, 9, 15, 21, 25, 10, 8, 26];
-
-
-
+//v4 замена цыфр на соответствующие гласные буквы
+let x = [118, 117, 120, 121, 117, 98, 122, 97, 120, 106, 104, 116, 113, 114, 113, 120, 106];
+let y = [101, 121, 110, 113, 113, 103, 121, 121, 101, 107, 103];
+function swich(num){
 for (let i = 0; i < num.length; i++)
 {
 
-if(num[i] == 1){
-num[i] = 'А';
+if(num[i] == 97){
+num[i] = 'a';
 }
 
-else if(num[i] == 5){
-    num[i] = 'Е';
+else if(num[i] == 101){
+    num[i] = 'e';
     }
 
-    else if(num[i] == 9){
-        num[i] = 'I';
+    else if(num[i] == 105){
+        num[i] = 'i';
         }
     
-        else if(num[i] == 15){
-            num[i] = 'О';
+        else if(num[i] == 111){
+            num[i] = 'o';
             }
 
-            else if(num[i] == 21){
-                num[i] = 'U';
+            else if(num[i] == 117){
+                num[i] = 'u';
                 }
 
-                else if(num[i] == 25){
-                    num[i] = 'Y';
-                    }
-                              
+                else if(num[i] == 121){
+                    num[i] = 'y';
+                    }                            
 }
 console.log(num);
+}
+
+swich(y);
 
 
 
--- lab1/R1.2.js
+++ lab1/R1.2.js
@@ -1,18 +1,30 @@
 //Таблица умножения NxN размера указаного в пораметре
 //multiplicationTable(3)  ->[[1,2,3],[2,4,6],[3,6,9]]
 
-let arr = new Array();
-let table = new Array();
-let num = prompt("Введите число:  ")
 
-function mult() {
 
-    for (let i = 0; i < num; i++) {
-        arr[i] = i+1;
-        
+
+function multiplicationTable()
+ {
+
+  let table = "";
+  let N = prompt("Введите число:  ")
+
+      for(let x = 1; x <= N; x++)
+      {
+
+        for(let y = 1; y <= N; y++)
+        {
+
+            table += x*y + " ";
+
+        }
+
+        table += "\n";
       }
-      console.log(arr);
-  }
 
-  mult();
+      console.log(table);
+ }
+
+ multiplicationTable();
   
