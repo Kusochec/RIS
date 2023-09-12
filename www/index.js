787a8ab77f98478f7e423d7996e09c08b9df1b67
BERDNIKAU YAUHENI <132736598+Kusochec@users.noreply.github.com>
2023-09-12 08:49:21 +0300

    reorg

--- www/index.js
+++ www/index.js
@@ -1,9 +1,15 @@
 /*let num = 5
 console.log (`variable: ${num}`); //кавычка находиться на букве ё*/
 
+
 const CC = require('currency-converter-lt');
 let currencyConverter = new CC({from:"USD", to:"BYN", amount:1})
 
 currencyConverter.convert().then((response) => {
     console.log (`Resualt: ${response}`)
 })
+
+
+/*const os =require('os')
+let res = os.platform()
+console.log(res)*/
