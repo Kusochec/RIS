8dc84cd0863063da8c1618d89e55db17a5a11067
BERDNIKAU YAUHENI <132736598+Kusochec@users.noreply.github.com>
2023-09-15 11:55:46 +0300

    itproger 6,7,8

--- www/index.js
+++ www/index.js
@@ -44,10 +44,56 @@ console.log('Всё сработало')
 
 
 //Работа с папками
-const fs = require('fs')
-//fs.mkdirSync('text-files')
+/*const fs = require('fs')
+//fs.mkdirSync('text-files') //сохздание папки 'text-files'*/
 
+//Создание
+/*const fs = require('fs')
 fs.mkdir('text-files', () =>{
-    
+  fs.writeFile('./text-files/some.txt', 'Hello', () => {///создание файла 'some.txt'запись в него Hello
+}) 
+})*/
+
+//Удоление
+/*const fs = require('fs')
+fs.unlink('./text-files/some.txt',() => { //Удалялка для файлов
+    fs.rmdir('./text-files', () => { }) //Удалялка для папок
+})*/
+
+
+//Создание сервера 
+const http = require ('http')
+const fs = require('fs')
+
+let server = http.createServer((req, res) => { //Cоздание сервера 
+//"req"- запрос пользоватиля / "res" - ответ сервера
+//Коды ответов Http
+//Информационные 100 - 199
+//Успешные 200 - 299
+//Перенаправления 300 - 399
+//Клиентские ошибки 400 - 499
+//Серверные ошибки 500 - 599
+res.writeHead(200,{'Content-Type': 'text/html; charset =utf-8'}) //Заголовки для страницы. Первый пораметр это Код ответа Http.
+//Второй: Тип отаброжаемого контента (plain или html) c кодировкой utf-8
+//res.end() //вывод информации на сираницу
+
+if(req.url =='/')
+fs.createReadStream ('./templates/index.html')// Считование информации в потоке данных
+//Может считывать информацию по чючють тем самым постепенно подгружать страницу
+.pipe(res) //Отправляет подгруженую иформацию на сервер
+
+else if (req.url == '/about')
+fs.createReadStream ('./templates/about.html').pipe(res)
+
+else
+fs.createReadStream ('./templates/error.html').pipe(res)
 })
+
+const PORT = 8080
+const HOST = 'localhost' //вместа 'localhost' можно прописать 127.0.0.1
+
+server.listen(PORT, HOST, ()=>{ 
+console.log (`Сервер запущен: http://${HOST}:${PORT}`)    
+})
+
 
