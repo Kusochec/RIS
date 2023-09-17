173753ecf8cc1cbc27b6b73fde779318218f998b
BERDNIKAU YAUHENI <132736598+Kusochec@users.noreply.github.com>
2023-09-17 19:46:52 +0300

    Itproger 10

--- WWW express/index.js
+++ WWW express/index.js
@@ -14,22 +14,29 @@ const express = require('express')
     res.sendFile(__dirname + '/index.html') //Указали фаел для открытия на главную страницу
    }) //__dirname - это константа для получения полного пути к папке */
 
+   //страница с денамическим url адресом 
+ /*app.get('/user/:username/:id', (req, res) => { // Денамический адрес с пораметрами "username" и "id".
+    // Она принемает эти пораметры от ползователя который введёт их адресную страку пример: "http://localhost:8080/Joni/233"
+    res.send(`User ID: ${req.params.id}. Username: ${req.params.username}`) // обращение к деномическому пораметру "id" и "usrtname"
+    // для вывода их на экран 
+   })*/
+
    app.get('/', (req, res) => { 
     res.render('index') //Подключение папки через функцию шаблонизатора 
    // Для исползования этой функции тебуеться прерместить 
    //в папку "viws" index.html и переиминовать его в шindex.ejs
    })
 
- app.get('/about', (req, res) => { // отслеживани страници /about (URL адреса)
-    res.send('Это страница about') // Что далжно быть отображено на главной странице
+   app.get('/about', (req, res) => { 
+    res.render('about')
    })
 
-   app.get('/user/:username/:id', (req, res) => { // Денамический адрес с пораметрами "username" и "id".
-    // Она принемает эти пораметры от ползователя который введёт их адресную страку пример: "http://localhost:8080/Joni/233"
-    res.send(`User ID: ${req.params.id}. Username: ${req.params.username}`) // обращение к деномическому пораметру "id" и "usrtname"
-    // для вывода их на экран 
+   app.get('/user/:username', (req, res) => { 
+    let data = {username: req.params.username, hobbis:['futboll', 'skate', 'Basketball']}//1) это объект со свойством 'username:' и значения этого свойства 'req.params.username'
+      // 2) Массив данных hobbis:
+    res.render('user', data) // Мы передали объекты username: и hobbis: странице "user"
    })
-
+   
  const PORT = 8080 //порт в костанте
 
  app.listen (PORT, () =>{// создание самого сервера
